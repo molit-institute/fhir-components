8aab62d873572adac2f384a5e793d6d509eebe73
Daniel Zsebedits <daniel.zsebedits@molit.eu>
2019-07-30 16:49:58 +0200

    added test page

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
-  <router-view />
+  <div>
+    <nav class="navbar navbar-light bg-light">
+      <span class="navbar-brand mb-0 h1">@molit/fhir-components</span>
+    </nav>
+    <router-view />
+  </div>
 </template>
 
 <style lang="scss">
---
+++ src/views/Test.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <resource-selector :fhirBaseUrl="baseUrl" :resourceName="'Patient'" :titleAttribute="titleAttribute" :searchAttributes="searchAttributes" />
+  </div>
+</template>
+
+<script>
+import ResourceSelector from "@/components/ResourceSelector";
+
+export default {
+  data() {
+    return {
+      baseUrl: "https://fhir.molit.eu/r4/",
+      searchAttributes: [
+        {
+          name: "Name",
+          value: "name"
+        }
+      ],
+      titleAttribute: {
+        value: "name",
+        type: "HumanName"
+      }
+    };
+  },
+
+  components: {
+    ResourceSelector
+  }
+};
+</script>
