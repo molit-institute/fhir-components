850e60ea60b375a362493698051bc5ed0d8b5167
Katharina Zeller <Katharina.Zeller@molit.eu>
2020-11-17 12:20:01 +0100

    Changed use field to not be shown as default

--- src/components/data-types/complex/DtHumanName-de-basis.vue
+++ src/components/data-types/complex/DtHumanName-de-basis.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <div v-if="edit">
+      <div class="form-row" v-if="viewUse">
+        <dt-code label="Use" class="col-md-12" :value.sync="resourceData.use" :valueSet="nameUse" :edit="edit" />
+      </div>
       <div class="form-row">
         <dt-string :label="prefix" class="col-md-2" :value.sync="resourceData.prefix" :edit="edit" />
         <dt-string-array :label="firstname" class="col-md-4" :value.sync="resourceData.given" :edit="edit" />
@@ -21,15 +24,20 @@
         {{ getDisplayForArray(resourceData.given) }} {{ resourceData.family }}
         {{ getDisplayForArray(resourceData.suffix) }}
       </div>
+      <div class="text-muted" v-if="viewUse">
+        {{ getDisplayForCode(nameUse, resourceData.use) }}
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import BaseDataType from "./BaseDataType";
+import DtCode from "./../primitive/DtCode";
 import DtString from "./../primitive/DtString";
 import DtStringArray from "./../primitive/DtStringArray";
 
+import nameUse from "@/assets/fhir/value-sets/name-use.json";
 
 export default {
   extends: BaseDataType,
@@ -45,9 +53,17 @@ export default {
     prefix: {
       type: String,
       default: "Prefix"
+    },
+    viewUse: {
+      type: Boolean,
+      default: false
     }
   },
   computed: {
+    nameUse() {
+      return nameUse;
+    }
+
     // text() {
     //   return this.resourceData
     //     ? this.resourceData.prefix +
@@ -66,6 +82,14 @@ export default {
   },
 
   methods: {
+    getDisplayForCode(valueSet, code) {
+      let valueSetItem = valueSet.expansion.contains.find(v => v.code === code);
+      if (valueSetItem) {
+        return valueSetItem.display;
+      }
+      return undefined;
+    },
+
     getDisplayForArray(array) {
       if (array && Array.isArray(array)) {
         return array.join(" ");
@@ -76,6 +100,7 @@ export default {
   },
 
   components: {
+    DtCode,
     DtString,
     DtStringArray
   }
